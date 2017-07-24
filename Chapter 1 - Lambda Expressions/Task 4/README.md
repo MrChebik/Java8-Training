ed71e7c3fc48f6218f8cc4219b66a13a37ff9d67
mrchebik <mrchebik@yandex.ru>
2017-07-24 04:42:31 -0400

    Added Task 4 from Chapter 1.

---
+++ Chapter 1 - Lambda Expressions/Task 4/README.md
@@ -0,0 +1,3 @@
+# Task 4
+Given an array of `File` objects, sort it so that directories come before the files, and within each group, elements are 
+sorted by path name, Use a lambda expression, not a `Comparator`.
---
+++ Chapter 1 - Lambda Expressions/Task 4/src/main/java/ru/mrchebik/Main.java
@@ -0,0 +1,12 @@
+package ru.mrchebik;
+
+import ru.mrchebik.directory.Directory;
+
+/**
+ * Created by mrchebik on 7/24/17.
+ */
+public class Main {
+    public static void main(String[] args) {
+        Directory.sortAllFiles("/home/mrchebik/idea-IU-171.4424.56");
+    }
+}
