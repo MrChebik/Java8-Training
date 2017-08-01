41a3a0a075d908c39b5594c44ae6a5f4d0ebfbbb
mrchebik <mrchebik@yandex.ru>
2017-08-01 15:18:43 -0400

    Added Task 3 to Chapter 2.

--- Chapter 2 - The Stream API/README.md
+++ Chapter 2 - The Stream API/README.md
@@ -8,3 +8,8 @@ result as they come in. (You don't want the threads to update a single counter. 
 ## Task 2
 Verify that asking for the first five long words does not call the filter method once the fifth long word has been 
 found. Simply log each method call.
+
+## Task 3
+Measure the difference when counting long words with a `parallelStream` instead of a `stream`. Call 
+`System.currentTimeMillis` before and after the call, and print the difference. Switch to a larger document (such as 
+_War and Peace_) if you have a fast computer.
---
+++ Chapter 2 - The Stream API/src/main/java/ru/mrchebik/task3/Main.java
@@ -0,0 +1,59 @@
+package ru.mrchebik.task3;
+
+import ru.mrchebik.Utils;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+/**
+ * Task:
+ *      Measure the difference when counting long words with a 'parallelStream' instead of a 'stream'. Call
+ *  'System.currentTimeMillis' before and after the call, and print the difference. Switch to a larger document (such as
+ *  _War and Peace_) if you have a fast computer.
+ *
+ * Created by mrchebik on 8/1/17.
+ */
+public class Main extends Utils {
+    private static final String PATH_TO_WAR_AND_PEACE = "/home/mrchebik/Documents/war-and-peace";
+
+    public static void main(String[] args) throws IOException {
+        File file = new File(PATH_TO_WAR_AND_PEACE);
+        List<String> text = new ArrayList<>();
+        List<String> parallelText = new ArrayList<>();
+        long start;
+        long startParallel;
+        long end;
+        long endParallel;
+
+        if (!file.exists()) {
+            start = System.currentTimeMillis();
+            Stream.of(generateMassive(1000, 24)).filter(word -> word.length() > 12).forEach(text::add);
+            end = System.currentTimeMillis() - start;
+
+            startParallel = System.currentTimeMillis();
+            Stream.of(generateMassive(1000, 24)).parallel().filter(word -> word.length() > 12).forEach(parallelText::add);
+            endParallel = System.currentTimeMillis() - startParallel;
+        } else {
+            start = System.currentTimeMillis();
+            Files.lines(file.toPath()).map(line -> line.replaceAll("^\\W+|\\W+$", "").split("\\W+")).filter(line -> !"".equals(line[0])).flatMap(Arrays::stream).filter(word -> word.length() > 12).forEach(text::add);
+            end = System.currentTimeMillis() - start;
+
+            startParallel = System.currentTimeMillis();
+            Files.lines(file.toPath()).parallel().map(line -> line.replaceAll("^\\W+|\\W+$", "").split("\\W+")).filter(line -> !"".equals(line[0])).flatMap(Arrays::stream).filter(word -> word.length() > 12).forEach(parallelText::add);
+            endParallel = System.currentTimeMillis() - startParallel;
+        }
+
+        System.out.println("General   : " + end + "ms\n" +
+                "Parallel  : " + endParallel + "ms\n" +
+                "Difference: " + (end - endParallel) + "ms" +
+                "\n=====\n" +
+                text + "\n" +
+                parallelText + "\n" +
+                "Equals: " + text.equals(parallelText));
+    }
+}
