1205a708c2a3ca88095756780dbf671424e8482b
mrchebik <mrchebik@yandex.ru>
2017-08-12 16:22:50 +0300

    Added Task 1 to Chapter 3.

---
+++ Chapter 3 - Programming with Lambdas/README.md
@@ -0,0 +1,6 @@
+# Chapter 3 - Programming with Lambas
+
+## Task 1
+Enhance the lazy logging technique by providing conditional logging. A typical call would be 
+`logIf(Level.FINEST, () -> i == 10, () -> "a[10] = " + a[10])`. Don't evaluate the condition if the logger won't log 
+the message.
---
+++ Chapter 3 - Programming with Lambdas/src/main/java/ru/mrchebik/task1/Main.java
@@ -0,0 +1,38 @@
+package ru.mrchebik.task1;
+
+import ru.mrchebik.Utils;
+
+import java.io.IOException;
+import java.util.function.Supplier;
+import java.util.logging.Level;
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
+import java.util.stream.IntStream;
+
+/**
+ * Task:
+ *      Enhance the lazy logging technique by providing conditional logging. A typical call would be
+ *  'logIf(Level.FINEST, () -> i == 10, () -> "a[10] = " + a[10])'. Don't evaluate the condition if the logger won't log
+ *  the message.
+ *
+ * Created by mrchebik on 8/12/17.
+ */
+public class Main extends Utils {
+    private static Logger logger = Logger.getLogger(Main.class.getName());
+
+    public static void main(String[] args) throws IOException {
+        LogManager.getLogManager().readConfiguration(Main.class.getResourceAsStream("/logging.properties"));
+
+        logger.setLevel(Level.FINEST);
+
+        String[] a = generateMassive(20, 5);
+
+        IntStream.range(0, a.length).forEach(i -> logIf(Level.FINEST, () -> i == 10, () -> "a[10] = " + a[i]));
+    }
+
+    private static void logIf(Level level, Supplier<Boolean> condition, Supplier<String> message) {
+        if (logger.isLoggable(level) && condition.get()) {
+            logger.finest(message.get());
+        }
+    }
+}
