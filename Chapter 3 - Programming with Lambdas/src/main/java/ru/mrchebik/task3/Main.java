07103967a9534626ef62b82780542e4a9c1d938b
mrchebik <mrchebik@yandex.ru>
2017-08-13 14:54:02 +0300

    Update description of Task 3 from Chapter 3.

--- Chapter 3 - Programming with Lambdas/src/main/java/ru/mrchebik/task3/Main.java
+++ Chapter 3 - Programming with Lambdas/src/main/java/ru/mrchebik/task3/Main.java
@@ -4,7 +4,7 @@ import java.util.function.BooleanSupplier;
 
 /**
  * Task:
- *      Java 1.4 added assertions to the language, with an `assert` keyword. Why were assertions not supplied as a
+ *      Java 1.4 added assertions to the language, with an 'assert' keyword. Why were assertions not supplied as a
  *  library feature? Could they be implemented as a library feature in Java 8?
  *
  * Created by mrchebik on 8/13/17.
